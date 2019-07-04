a70d16c5f338bd4a13920984d92cab3655278976
leonov-pavel-msk <leonov.pavel.msk@gmail.com>
2019-07-04 16:16:20 +0300

    Comments

---
+++ php/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comments</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+		<div class="header wrapper">
+		<div class="info-header">
+			<div class="info-p">
+				<p>Телефон: (499) 340-94-71</p>
+				<p>Email: info@future-group.ru</p>
+			</div>
+			<h1 class="h1-header">Комментарии</h1>
+		</div>
+		<div class="logo-header"></div>
+		</div>
+    </header>
+    
